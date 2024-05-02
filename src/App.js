b9848cc8b5515521b68a81695186c0164f2395af
Maulik Kodala <72224079+maulikkodala@users.noreply.github.com>
2024-05-02 11:56:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+
+
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+
+    setAlert({ msg: message, type: type })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+
+      setMode('dark')
+      showAlert("Dark Mode Enable", "success")
+    } else {
+      setMode('light')
+      showAlert("Light Mode Enable", "success")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar title="Text Utils" abouttext="About" mode={mode} togglemode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Text To Analyze" mode={mode} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
